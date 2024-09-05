bfec25d89263eb74438387a602a8feda20f24d2b
RaeezLachporia <88308343+RaeezLachporia@users.noreply.github.com>
2024-09-05 12:59:27 +0200

    Working NavMesh

---
+++ MidNightBastion2/Assets/Scripts/Enemies Scripts/EnemyNavigation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyNavigation : MonoBehaviour
+{
+    [SerializeField] private Transform movePostionTransform;
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        agent.destination = movePostionTransform.position;
+    }
+}
